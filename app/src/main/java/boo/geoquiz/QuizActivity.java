402db9a2fa7346d4ad82cef7422c52c769996e06
BOO1980 <hbillingham80@gmail.com>
2016-06-29 13:23:25 +0100

    Chapter 1 - Listing 1.9 - Set listener for True button

--- app/src/main/java/boo/geoquiz/QuizActivity.java
+++ app/src/main/java/boo/geoquiz/QuizActivity.java
@@ -4,10 +4,12 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 
 //Listing 1.4 - Default Class file for QuizActivity.java
 //Listing 1.7 - Adding member variables QuizActivity.java
+//Listing 1.8 - Getting references to widgets QuizActivity.java
 public class QuizActivity extends AppCompatActivity {
 
     private Button mTrueButton;
@@ -19,6 +21,13 @@ public class QuizActivity extends AppCompatActivity {
         setContentView(R.layout.activity_quiz);
 
         mTrueButton = (Button) mTrueButton;
+        mTrueButton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                //does nothing yet
+            }
+        });
         mFalseButton = (Button) mFalseButton;
     }
 
